7e83cb60dc8760aebdadef505fcb12065d7fb553
mac <mac@mactekiMac-2.local>
2013-12-24 09:39:04 +0800

    Initial Commit

---
+++ HWeShare/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HWeShare
+//
+//  Created by mac on 13-12-24.
+//  Copyright (c) 2013年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HWeShareTests/HWeShareTests.h
@@ -0,0 +1,13 @@
+//
+//  HWeShareTests.h
+//  HWeShareTests
+//
+//  Created by mac on 13-12-24.
+//  Copyright (c) 2013年 mac. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HWeShareTests : SenTestCase
+
+@end
