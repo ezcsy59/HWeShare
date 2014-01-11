5379eca66f04d1c131cb9b9bdf93b0c9b30738a4
ezcsy59 <592482931@qq.com>
2014-01-11 18:19:30 +0800

    1  11

--- HWeShare/DetailVC.m
+++ HWeShare/DetailVC.m
@@ -10,6 +10,7 @@
 #import "AppDelegate.h"
 #import "SDImageView+SDWebCache.h"
 #import "WebVC.h"
+#import "KxMenu.h"
 @interface DetailVC ()
 
 @end
@@ -44,11 +45,56 @@
     self.ti.text = [myDelegate.detail objectForKey:@"title"];
     NSURL *url=[NSURL URLWithString:[myDelegate.detail objectForKey:@"pic_url" ] ];
     [self.image setImageWithURL:url ];
+    UIBarButtonItem *btnCancel = [[UIBarButtonItem alloc]
+                                  initWithTitle:@"分享"
+                                  style:UIBarButtonItemStyleBordered
+                                  target:self
+                                  action:@selector(showMenu:)];
+   
+    self.navigationItem.rightBarButtonItem = btnCancel;
     
     
 }
 
+- (void)showMenu:(UIBarButtonItem *)sender
+{
+    
+    NSArray *menuItems =
+    @[
+      
+      [KxMenuItem menuItem:@"分享菜单"
+                     image:nil
+                    target:nil
+                    action:NULL],
+      
+      [KxMenuItem menuItem:@"分享给微信朋友"
+                     image:nil
+                    target:self
+                    action:@selector(pushMenuItem:)],
+      
+      [KxMenuItem menuItem:@"分享到微信朋友圈"
+                     image:nil
+                    target:self
+                    action:@selector(pushMenuItem:)],
+      
+      [KxMenuItem menuItem:@"分享到分享平台"
+                     image:nil
+                    target:self
+                    action:@selector(pushMenuItem:)],
+           ];
+    
+    KxMenuItem *first = menuItems[0];
+    first.foreColor = [UIColor colorWithRed:47/255.0f green:112/255.0f blue:225/255.0f alpha:1.0];
+    first.alignment = NSTextAlignmentCenter;
+    
+    const CGFloat H = self.view.bounds.size.height;
+    const CGFloat W = self.view.bounds.size.width;
+    
+    [KxMenu showMenuInView:self.view
+                  fromRect:CGRectMake(W - 40, 5, 0, 0)
+                 menuItems:menuItems];
 
+}
 
 - (void)didReceiveMemoryWarning
 {
